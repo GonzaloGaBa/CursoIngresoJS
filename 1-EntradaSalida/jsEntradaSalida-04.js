285de5d6257f3615a180e49e29bd3b98af106670
Gonzalo Barrientos <gongabar@gmail.com>
2023-01-31 16:12:49 -0300

    Dia 2

--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -1,9 +1,14 @@
 /* 
 Debemos lograr tomar un dato por 'PROMPT' 
 y lo muestro por 'ID' al presionar el botón 'mostrar'
-*/
+Gonzalo Barrientos-Div X*/
 function mostrar()
 {
-	alert("ok");
-}
+	
+	let  nombreIngresado;
 
+	 
+	nombreIngresado = prompt("Ingrese su nombre de Usuario");
+
+	document.getElementById("txtIdNombre").value = nombreIngresado;
+}
