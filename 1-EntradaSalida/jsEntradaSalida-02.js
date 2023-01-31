766f95ec9ec25b2a83dd29782aee25e02f4909de
Gonzalo Barrientos <gongabar@gmail.com>
2023-01-31 15:05:17 -0300

    Comit de clase 1

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -1,8 +1,16 @@
 /*
 Debemos lograr tomar un nombre con 'prompt' 
-y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+y luego mostrarlo por 'alert' al presionar el botón  'mostrar'
+Gonzalo Barrientos-Div X- */
 function mostrar()
 {
-	alert("ok");
+	let nombre; //declaracion de variable
+	//asignación
+	//nombre = "Yanina"
+
+	nombre = prompt("Ingrese su nombre");
+
+
+	alert("El usuario se llama" + nombre);
 }
 
