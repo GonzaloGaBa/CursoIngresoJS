a066013b8c074fd0bf6d8537f10a0d6cb77fc749
GonzaloGaBa <gongabar@gmail.com>
2023-02-23 10:47:00 -0300

    switch

--- 3-InstruccionSwitch/jsInstruccionSwitch-04.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-04.js
@@ -1,11 +1,20 @@
 function mostrar()
 {
 	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
-	
-	
-
-
-
+	let mesDelAño;
+	mesDelAño = document.getElementById("txtIdMes").value;
+	switch (mesDelAño) {
+		case "Febrero":
+			alert("Tiene 28 dias.")
+			break;
+		case "Abril":
+		case "Junio":
+		case "Septiembre":
+		case "Noviembre":
+			alert("Tiene 30 dias.")
+			break;
+		default:
+			alert("Tiene 31 dias.")
+			break;
+	}
 }//FIN DE LA FUNCIÓN
